d0e56749cf6714420434324df7209177d89ddc56
Ryan Prince <ryanprince@macbook-pro.myfiosgateway.com>
2022-10-08 16:33:02 -0400

    first commit

---
+++ CupcakeCornerV2/CupcakeCornerV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerV2App.swift
+//  CupcakeCornerV2
+//
+//  Created by Ryan Prince on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
